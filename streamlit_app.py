0c469b422dd03078433eb696f3c30fb319753bca
Sritam Roy Chowdhury <90959767+SritamRC@users.noreply.github.com>
2026-01-29 21:10:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ ingrediant_string= ''
 if ingredients_list:
     for each_fruit in ingredients_list:
        ingrediant_string+= each_fruit +' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)    
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)    
 
 
     
